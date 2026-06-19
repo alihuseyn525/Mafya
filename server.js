cacec6e8fbe898181a3a032c7feae93818aaa980
alihuseyn525 <alihuseyn525@gmail.com>
2026-06-19 16:55:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -657,7 +657,6 @@ io.on("connection", socket => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));  return a;
-}
 
 // ─── Otaqlar ──────────────────────────────────────────────
 const rooms = {};
